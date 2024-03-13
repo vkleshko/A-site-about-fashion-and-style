aaa6953b2f33c941b97a51ef2ed18a1c6c61d808
Vlad Kleshko <vkoesko@gmail.com>
2024-03-13 23:50:47 +0200

    Created admin panel

--- fashion/admin.py
+++ fashion/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from fashion.models import Item, Category
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 1
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
