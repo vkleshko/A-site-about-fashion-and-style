f8f7c72b7c8fd1ee4f912b1e0770fc8afcafea32
Vlad Kleshko <vkoesko@gmail.com>
2024-03-13 22:41:49 +0200

    Created Item models & upload_to function

--- fashion/models.py
+++ fashion/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def item_image_file_path(instance, file_name):
+    _, extension = os.path.splitext(file_name)
+    file_path = f"{slugify(instance.name)}-{uuid.uuid4()}.{extension}"
+    return os.path.join("uploads/items/", file_path)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    picture = models.ImageField(upload_to=item_image_file_path)
