54937f229d4904c353cb7e56f2f5e25984d3ede2
Vlad Kleshko <vkoesko@gmail.com>
2024-03-13 22:53:37 +0200

    Created index view

--- fashion/views.py
+++ fashion/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from fashion.models import Item
+
+
+def index(request):
+    items = Item.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "items": items,
+        "num_visits": num_visits,
+    }
+
+    return render(request, "fashion/index.html", context)
