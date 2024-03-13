593741e293404b38b5e22418b0ebc98444ba9496
Vlad Kleshko <vkoesko@gmail.com>
2024-03-14 01:16:08 +0200

    Created 'categories/' url

--- fashion/urls.py
+++ fashion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from fashion.views import index
+from fashion.views import index, CategoryListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("categories/", CategoryListView.as_view(), name="category-list"),
 
 ]
 app_name = "fashion"
