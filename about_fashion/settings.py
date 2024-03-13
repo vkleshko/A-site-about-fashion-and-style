da80d4b5b97c0f65ba6d7639ee72ba08316ba0d6
Vlad Kleshko <vkoesko@gmail.com>
2024-03-13 23:20:40 +0200

    Configured static files

--- about_fashion/settings.py
+++ about_fashion/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "about_fashion.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -111,6 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
