2de424b2957a76c4f78edf75e09f032f90199430
Vlad Kleshko <vkoesko@gmail.com>
2024-03-14 17:03:21 +0200

    Created 'categories/business/' url

--- fashion/urls.py
+++ fashion/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from fashion.views import index, CategoryListView
+from fashion.views import (
+    index,
+    CategoryListView,
+    BusinessWearListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("categories/", CategoryListView.as_view(), name="category-list"),
+    path("categories/business/", BusinessWearListView.as_view(), name="business-wear"),
 
 ]
 app_name = "fashion"
